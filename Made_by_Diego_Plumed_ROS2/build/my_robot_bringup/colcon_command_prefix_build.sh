2e993564c615a30d94b9da82693617a38ee39ef3
Diego Plumed Beortegui <diego@dyn-59-191-215-190.its.monash.edu.au>
2026-01-16 11:46:51 +1100

    Reorder the folders

--- Made_by_Diego_Plumed_ROS2/build/my_robot_bringup/colcon_command_prefix_build.sh
+++
@@ -1,5 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/diego/ros2_ws/install/my_cpp_pkg/share/my_cpp_pkg/package.sh"
-. "/home/diego/ros2_ws/install/my_robot_interfaces/share/my_robot_interfaces/package.sh"
-. "/home/diego/ros2_ws/install/my_py_pkg/share/my_py_pkg/package.sh"
-. "/home/diego/ros2_ws/install/turtlesim_catch_them_all/share/turtlesim_catch_them_all/package.sh"
